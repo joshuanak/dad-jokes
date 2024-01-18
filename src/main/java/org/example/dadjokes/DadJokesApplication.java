ae0d9b978a1b2510d969177cbbf394926253c8bd
joshuanakatani <joshua.nakatani@gmail.com>
2024-01-18 13:50:20 -0800

    initial commit

---
+++ src/main/java/org/example/dadjokes/DadJokesApplication.java
@@ -0,0 +1,13 @@
+package org.example.dadjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadJokesApplication.class, args);
+    }
+
+}
